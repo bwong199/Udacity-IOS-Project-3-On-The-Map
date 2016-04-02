98958d2bc9d1ea9fcceafdc61db67627a465f064
bwong199 <bwong199@my.bcit.ca>
2016-04-02 14:58:26 -0600

    Initial Commit

---
+++ Udacity On the Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udacity On the Map
+//
+//  Created by Ben Wong on 2016-04-02.
+//  Copyright © 2016 Ben Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
